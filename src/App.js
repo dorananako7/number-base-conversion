79840b2bffca8e1e75979d32bc611dde3948e493
dorananako8130 <dorananako8130@yushinnoiMac.local>
2026-05-23 17:20:20 +0900

    2進数から10進数への変換の実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [binary, setBinary] = useState('');
+    const [decimal, setDecimal] = useState('');
+    const [error, setError] = useState('');
 
-export default App;
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setBinary(value);
+
+        // 入力値が空の時はリセット
+        if (value === '') {
+            setDecimal('');
+            setError('');
+            return;
+        }
+
+        // 例外処理
+        if(!/^[01]+$/.test(value)) {
+            setError('エラー：0と1以外が入力されています。');
+            setDecimal('');
+            return;
+        }
+        
+        // 変換処理
+        setError('');
+        const decimalNumber = parseInt(value, 2);
+        setDecimal(decimalNumber.toString());
+    };
+    return (
+        <>
+        <h1>2進数→10進数</h1>
+        <input
+            type="text"
+            value={binary}
+            onChange={handleChange}
+            placeholder="2進数を入力してください"
+        />
+        {error !== '' ? <p>{error}</p> : null}
+        <input 
+            type="text"
+            value={decimal}
+            readOnly
+            placeholder="10進数が表示されます"
+        />
+        </>
+    );
+    }
+
+    // このファイルを読み込んだら、デフォルトでAppコンポーネントを渡すという設定
+    export default App;
