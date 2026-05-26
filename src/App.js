d36d1374abbba061034cde784d249e6e025119cc
dorananako8130 <dorananako8130@yushinnoiMac.local>
2026-05-26 15:18:17 +0900

    10進数から2進数への変換機能実装

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 function TwoToTen() {
     const [binary, setBinary] = useState('');
@@ -17,12 +17,12 @@ function TwoToTen() {
         }
 
         // 例外処理
-        if(!/^[01]+$/.test(value)) {
+        if (!/^[01]+$/.test(value)) {
             setError('エラー：0と1以外が入力されています。');
             setDecimal('');
             return;
         }
-        
+
         // 変換処理
         setError('');
         const decimalNumber = parseInt(value, 2);
@@ -30,66 +30,82 @@ function TwoToTen() {
     };
     return (
         <>
-        <h1>2進数→10進数</h1>
-        <input
-            type="text"
-            value={binary}
-            onChange={handleChange}
-            placeholder="2進数を入力してください"
-        />
-        {error !== '' ? <p>{error}</p> : null}
-        <input 
-            type="text"
-            value={decimal}
-            readOnly
-            placeholder="10進数が表示されます"
-        />
+            <h1>2進数→10進数</h1>
+            <input
+                type="text"
+                value={binary}
+                onChange={handleChange}
+                placeholder="2進数を入力してください"
+            />
+            {error !== '' ? <p>{error}</p> : null}
+            <input
+                type="text"
+                value={decimal}
+                readOnly
+                placeholder="10進数が表示されます"
+            />
         </>
     );
-    }
+}
 
-    function TenToTwo() {
-        const [Ten, setTen] = useState('');
-        const [decimal, setDecimal] = useState('');
-        const [error, setReeoe] = useState('');
+function TenToTwo() {
+    const [Ten, setTen] = useState('');
+    const [decimal, setDecimal] = useState('');
+    const [error, setError] = useState('');
 
-        const handleChange = (e) => {
-            const value = e.target.value;
-            setTen(value);
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setTen(value);
 
-            if(value === '') {
-                setDecimal('');
-                setError('');
-                return ;
-            }
-
-            if(/^[0-9]+$/.test(value)) {
-                setError('エラー：数値を入力してください')
-                setDecimal('');
-                return;
-            }
-
+        //空白の時は何もしない
+        if (value === '') {
+            setDecimal('');
             setError('');
-            const decimalNumber = parseInt(value, 10).toString(2);
-            setDecimal(decimalNumber.toString());
-
-            
-
+            return;
         }
 
-        
-        
-    }
+        //例外処理
+        if (!/^[0-9]+$/.test(value)) {
+            setError('エラー：数値を入力してください')
+            setDecimal('');
+            return;
+        }
+
+        //10進数から2進数に変換
+        setError('');
+        const decimalNumber = parseInt(value, 10).toString(2);
+        setDecimal(decimalNumber.toString());
+    };
+    return (
+        <>
+         <h1>10進数→2進数</h1>
+            <input
+                type="text"
+                value={Ten}
+                onChange={handleChange}
+                placeholder="10進数を入力してください"
+            />
+            {error !== '' ? <p>{error}</p> : null}
+            <input
+                type="text"
+                value={decimal}
+                readOnly
+                placeholder="10進数が表示されます"
+            />
+        </>
+        );
+}
 
 
-    function App() {
-        return (
-            <>
+function App() {
+    return (
+        <>
             <h1> 基底変換ツール</h1>
             <TwoToTen />
-            </>
-        );
-    }
+            <TenToTwo />
+        </>
+    );
+}
 
-    // このファイルを読み込んだら、デフォルトでAppコンポーネントを渡すという設定
-    export default App;
+// このファイルを読み込んだら、デフォルトでAppコンポーネントを渡すという設定
+export default App;
