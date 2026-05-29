f2b0c00e931969adca93e0e314f3344704351c5e
dorananako8130 <dorananako8130@yushinnoiMac.local>
2026-05-30 02:53:41 +0900

    ページ遷移機能の追加

--- src/App.js
+++ src/App.js
@@ -1,91 +1,14 @@
-import {useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BaseConverter from './BaseConverter';
 
-function BaseConverter() {
-    const [sourceBase, setSourceBase] = useState('');
-    const [targetBase, setTargetBase] = useState('');
-    const [inputValue, setInputValue] = useState('');
-    const [result, setResult] = useState('');
-    const [error, setError] = useState('');
-
-    const handleSourceBaseChange = (e) => {
-        setSourceBase(e.target.value);
-        setInputValue('');
-        setResult('');
-        setError('');
-    };
-
-    const handleTargetBaseChange = (e) => {
-        setTargetBase(e.target.value);
-        setInputValue('');
-        setResult('');
-        setError('');
-    };
-
-    const handleChange = (e) => {
-        const value = e.target.value;
-        setInputValue(value);
-        if(value === '') {
-            setResult('');
-            setError('');
-            return ;
-        }
-
-        const decimalNumber = parseInt(value, parseInt(sourceBase));
-
-        if(isNaN(decimalNumber)) {
-            setError('エラー：変換できない文字が含まれています');
-            setResult('');
-            return ;
-        }
-
-        setError('');
-        const resultString = decimalNumber.toString(parseInt(targetBase));
-        setResult(resultString);
-    };
-
-    return(
-        <>
-        <h2>基数変換web.com</h2>
-        {/* 変換元の基数 */}
-        <input
-            type="number"
-            value={sourceBase}
-            onChange={handleSourceBaseChange}
-            min="2"
-            max="36"
-            placeholder="変換元の基数"
-            style={{
-                width: '95px',
-            }}
-        />
-        {/* 変換先の基数 */}
-        <input
-            type="number"
-            value={targetBase}
-            onChange={handleTargetBaseChange}
-            min="2"
-            max="36"
-            placeholder="変換先の基数"
-              style={{
-                width: '95px',
-            }}
-        />
-        {/* 変換元の値 */}
-        <input 
-            type="text"
-            value={inputValue}
-            onChange={handleChange}
-            placeholder="変換元の値"
-        />
-        {/* 結果表示 */}
-        <input
-            type="text"
-            value={result}
-            readOnly
-            placeholder="結果"
-        />
-        {error != '' ? <p>{error}</p> : null}
-        </>
-    );
+function App() {
+    return(<>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/number-base-conversion" element={<BaseConverter />} />
+            </Routes>
+        </BrowserRouter>
+    </>);
 }
-export default BaseConverter;
+
+export default App;
---
+++ src/BaseConverter.js
@@ -0,0 +1,91 @@
+import {useState} from 'react';
+
+function BaseConverter() {
+    const [sourceBase, setSourceBase] = useState('');
+    const [targetBase, setTargetBase] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [result, setResult] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSourceBaseChange = (e) => {
+        setSourceBase(e.target.value);
+        setInputValue('');
+        setResult('');
+        setError('');
+    };
+
+    const handleTargetBaseChange = (e) => {
+        setTargetBase(e.target.value);
+        setInputValue('');
+        setResult('');
+        setError('');
+    };
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value);
+        if(value === '') {
+            setResult('');
+            setError('');
+            return ;
+        }
+
+        const decimalNumber = parseInt(value, parseInt(sourceBase));
+
+        if(isNaN(decimalNumber)) {
+            setError('エラー：変換できない文字が含まれています');
+            setResult('');
+            return ;
+        }
+
+        setError('');
+        const resultString = decimalNumber.toString(parseInt(targetBase));
+        setResult(resultString);
+    };
+
+    return(
+        <>
+        <h2>基数変換web.com</h2>
+        {/* 変換元の基数 */}
+        <input
+            type="number"
+            value={sourceBase}
+            onChange={handleSourceBaseChange}
+            min="2"
+            max="36"
+            placeholder="変換元の基数"
+            style={{
+                width: '95px',
+            }}
+        />
+        {/* 変換先の基数 */}
+        <input
+            type="number"
+            value={targetBase}
+            onChange={handleTargetBaseChange}
+            min="2"
+            max="36"
+            placeholder="変換先の基数"
+              style={{
+                width: '95px',
+            }}
+        />
+        {/* 変換元の値 */}
+        <input 
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="変換元の値"
+        />
+        {/* 結果表示 */}
+        <input
+            type="text"
+            value={result}
+            readOnly
+            placeholder="結果"
+        />
+        {error != '' ? <p>{error}</p> : null}
+        </>
+    );
+}
+export default BaseConverter;
